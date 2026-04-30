f622c884f1291fddb7f0c31d0f98c8eea27cfba5
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-04-30 23:08:11 +0700

    add switch beserta 6 case dan 1 default

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -217,6 +217,29 @@ int main()
         cout << "Enter your choice: ";
 
         cin >> choice;
+
+        switch (choice)
+        {
+        case '1':
+            list.addNode();
+            break;
+        case '2':
+            list.hapus();
+            break;
+        case '3':
+            list.traverse();
+            break;
+        case '4':
+            list.revtraverse();
+            break;
+        case '5':
+            list.searchData();
+            break;
+        case '6':
+            return 0;
+        default:
+            cout << "Invalid option\n";
+        }
         
     }
 }
